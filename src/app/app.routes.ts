6b08b7b89c8a85c3d1a11cdd9abe0601a42248e4
Konrad Münch <konrad@munech.engineer>
2024-04-07 03:47:41 +0200

    changes in text and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { ServicesPageComponent } from './components/services-page/services-page.
 import { ImprintPageComponent } from './components/imprint-page/imprint-page.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/home-page', pathMatch: 'full' },
   { path: 'home-page', component: HomePageComponent },
   { path: 'service-page', component: ServicesPageComponent },
   { path: 'imprint-page', component: ImprintPageComponent }
